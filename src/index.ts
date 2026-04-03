b214c0f6b4394abdefc2380788047b805e59c8d5
Cristi <cristi@brigada.ro>
2026-04-03 22:06:56 +0300

    minor changes

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,6 @@ import type { API } from 'homebridge';
 import { ACCESSORY_NAME } from './settings';
 import { XiaomiMiAirPurifierAccessory } from './accessory';
 
-/**
- * This method registers the platform with Homebridge
- */
 export = (api: API) => {
   api.registerAccessory(ACCESSORY_NAME, XiaomiMiAirPurifierAccessory);
 };
--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,9 @@
 /**
  * This is the name of the accessory that users will use to register the plugin in the Homebridge config.json
  */
-export const ACCESSORY_NAME = 'XiaomiMiAirPurifierV2';
+export const ACCESSORY_NAME = 'AirPurifierXiaomi';
 
 /**
  * This must match the name of your plugin as defined in package.json
  */
-export const PLUGIN_NAME = 'homebridge-v2-xiaomi-mi-airpurifier';
+export const PLUGIN_NAME = 'homebridge-airpurifier-xiaomi';
